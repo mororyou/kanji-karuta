3c99e9b83b597362a2e0458bbde6287c52908ae3
R.Moroizumi <m.ryousuke0401@icloud.com>
2025-01-03 01:13:40 +0900

    :art: Add: create Elementaries constant for grade levels

---
+++ app/schemas/constants/elementaries.ts
@@ -0,0 +1,8 @@
+export const Elementaries = [
+  { name: '1年生', level: 1 },
+  { name: '2年生', level: 2 },
+  { name: '3年生', level: 3 },
+  { name: '4年生', level: 4 },
+  { name: '5年生', level: 5 },
+  { name: '6年生', level: 6 },
+];
